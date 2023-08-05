7498050061339d569d06b4d0744b24a66ade8591
Alexander <ural305@mail.ru>
2023-08-05 11:52:55 +0300

    proga

---
+++ C#/Program.cs
@@ -0,0 +1,62 @@
+﻿//See Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+//на само себя).
+//Например:
+//4 -> 16 
+//-3 -> 9 
+//-7 -> 49
+//Console.WriteLine("введите целое число");
+//int num = Convert.ToInt32( Console.ReadLine());
+//int square = num * num ;
+//Console.WriteLine($"Квадрат введенного числа :{square}");
+//if (num > 0)
+//{
+//    Console.WriteLine("Это положительное число");
+//} 
+//else if (num ==0)
+//    System.Console.WriteLine("Это ноль");
+//else
+//    Console.WriteLine("Число отрицательное");
+//int i = 1;
+//while (i <= 10)
+//{
+//    Console.Write($"{i} ");
+//    i ++ ; 
+//}
+//System.Console.WriteLine();
+
+// Console.Write("введите первое целое число :");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите второе целое число :");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int square = num1 * num1;
+// int square1 = num2 * num2;
+// if (num1 == square1)
+//     Console.WriteLine("Да это квадрат числа");
+// else
+//     Console.WriteLine("Нет не совпадают");
+
+// Console.Write("введите целое число :");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = -num ;
+// while (i <= num)
+// {
+//     Console.Write($"{i} ");
+//     i++ ; 
+// }
+// System.Console.WriteLine();
+
+// Console.Write("введите первое трехзначное целое число :");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = num % 10 ;
+// System.Console.WriteLine(i);
+
+Console.Write("введите любое целое число :");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = num %10 ;
+while (i >= i / 10 ) 
+    Console.WriteLine("Выводим третью цифру слева"); 
+    Console.WriteLine("Такой цифры нет");
+i-- ;
+System.Console.WriteLine(i);
+
+ 
