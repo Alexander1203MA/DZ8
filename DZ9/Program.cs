58c6a41f1353bee609ff53cf1981596e215636c6
Alexander <ural305@mail.ru>
2023-09-13 20:55:26 +0300

    Сделали ДЗ

--- DZ9/Program.cs
+++ DZ9/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите числа, через пробел: ");
 int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int count = 0;
- 
+
 for (int i = 0; i < arr.Length; i++)
 {
     if (arr[i] > 0)
@@ -9,27 +9,27 @@ for (int i = 0; i < arr.Length; i++)
         count++;
     }
 }
- 
+
 Console.WriteLine($"Количество чисел которые больше 0: {count}");
 
 
-Console.WriteLine ("Задача 43: Пересечение прямых y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine("Задача 43: Пересечение прямых y = k1 * x + b1, y = k2 * x + b2");
 
 // Создаем метод для ввода числа пользователем
 
-double InputNum (string message)
+double InputNum(string message)
 {
-    Console.Write (message);
-    return Convert.ToInt32 (Console.ReadLine ());
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
-double b1 = InputNum ($"Задайте b1: ");
-double k1 = InputNum ($"Задайте k1: ");
-double b2 = InputNum ($"Задайте b2: ");
-double k2 = InputNum ($"Задайте k2: ");
-    
+double b1 = InputNum($"Задайте b1: ");
+double k1 = InputNum($"Задайте k1: ");
+double b2 = InputNum($"Задайте b2: ");
+double k2 = InputNum($"Задайте k2: ");
+
 
 double x = (b2 - b1) / (k1 - k2);
 double y = k1 * (b2 - b1) / (k1 - k2) + b1;
 
-Console.WriteLine ($"Точка пересечения прямых: ({x}, {y})");
+Console.WriteLine($"Точка пересечения прямых: ({x}, {y})");
