453e886900ba51c0193ae5a6f205e2a6703f1956
Alexander <ural305@mail.ru>
2023-09-22 20:58:54 +0300

    Сделали ДЗ

--- DZ12/Program.cs
+++ DZ12/Program.cs
@@ -4,75 +4,78 @@ int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
 NaturalToNum(number, count);
 
-void NaturalToNum(int n, int count)
+void NaturalToNum(int t, int count)
 {
-    if (count > n)
+    if (count > t)
     {
         return;
     }
     else
     {
-        NaturalToNum(n, count + 1);
+        NaturalToNum(t, count + 1);
         Console.Write(count + " ");
     }
 }
+Console.WriteLine();
+Console.WriteLine();
 
 
-// System.Console.WriteLine("Задача 66");//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-// Console.Write("Введите число M: ");
-// int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Задача 66");//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
-// Console.Write("Введите число N: ");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-// SumFromMToN(m, n);
+SumFromMToN(m, n);
 
-// void SumFromMToN(int m, int n)
-// {
-//     Console.Write(SumMN(m - 1, n));
-// }
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
 
-// int SumMN(int m, int n)
-// {
-//     int res = m;
-//     if (m == n)
-//         return 0;
-//     else
-//     {
-//         m++;
-//         res = m + SumMN(m, n);
-//         return res;
-//     }
-// }
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+Console.WriteLine();
+Console.WriteLine();
+
+System.Console.WriteLine("Задача 68");// Напишите программу вычисления функции Аккермана с помощью рекурсии
+Console.Write("Введите число M: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(z,x);
 
 
-// System.Console.WriteLine("Задача 68");// Напишите программу вычисления функции Аккермана с помощью рекурсии
-// Console.Write("Введите число M: ");
-// int m = Convert.ToInt32(Console.ReadLine());
+void AkkermanFunction(int z, int x)
+{
+    Console.Write(Akkerman(z, x)); 
+}
 
-// Console.Write("Введите число N: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-
-// AkkermanFunction(m,n);
-
-
-// void AkkermanFunction(int m, int n)
-// {
-//     Console.Write(Akkerman(m, n)); 
-// }
-
-// int Akkerman(int m, int n)
-// {
-//     if (m == 0)
-//     {
-//         return n + 1;
-//     }
-//     else if (n == 0 && m > 0)
-//     {
-//         return Akkerman(m - 1, 1);
-//     }
-//     else
-//     {
-//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
-//     }
-// }
+int Akkerman(int z, int x)
+{
+    if (z == 0)
+    {
+        return x + 1;
+    }
+    else if (x == 0 && z > 0)
+    {
+        return Akkerman(z - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(z - 1, Akkerman(z, x - 1)));
+    }
+}
